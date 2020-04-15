8e2a8ab175dda3cbe87e26256fbcff0f672cba1a
Barton Willis <willisb@unk.edu>
2020-04-15 06:00:10 -0500

    Update in-domain.lisp
    o add comments to in-domain.lisp
    
    o provide optional  implementation dependent arguments to make-hash table.
    
    o 72 tests failed out of 16,807 total tests.
    Evaluation took:
      521.448 seconds of real time
      492.296875 seconds of total run time (458.515625 user, 33.781250 system)
      [ Run times consist of 24.019 seconds GC time, and 468.278 seconds non-GC time. ]
      94.41% CPU
      441,618,716 forms interpreted
      1,378 lambdas converted
      1,414,170,664,842 processor cycles
      157,110,496,240 bytes consed

--- in-domain.lisp
+++ in-domain.lisp
@@ -6,7 +6,7 @@
 
 (in-package :maxima)
 
-(defvar *in-domain* (make-hash-table))
+(defvar *in-domain* (make-hash-table :size 16 :rehash-size 1))
 
 (defun unlike (a b)
 	(not (alike1 a b)))
@@ -31,12 +31,10 @@
 
 		 (list '%sec #'(lambda (q) (take '(mand) (not-zerop (take '(%cos) q)) (in-domain q))))
 
-
 		 (list '%atan #'(lambda (q) (take '(mand)
 										   (unlike q '$%i)
 										   (unlike q (mul -1 '$%i))
 										   (in-domain q))))
-
 
 		 (list '%acot #'(lambda (q) (take '(mand)
 										   (unlike q '$%i)
@@ -60,8 +58,26 @@
 (defun $indomain(e x)
 	(in-domain e (cdr x)))
 
+;;; Return a Maxima predicate (Boolean valued expression) that is true on the
+;;; complex domain of e and false off the complex domain. A condition that is
+;;; independent of the variables in the CL list are assumed to be true.
+;;; Examples: in-domain(x - a/b, (x)) --> x and in-domain(x - a/b) --> b # 0.
+
+;;; This function only considers the complex domain, so in-domain(log(x)) --> x # 0,
+;;; not something like x > 0.
+
+;;; For functions, such as cosecant, in-domain returns sin(x) # 0. Although it would
+;;; be lovely to return something like not integerp(x/pi), Maxima doesn't know all
+;;; that much about integerp, and integerp isn't a simplifying function.
+
+;;; For a function that isn't listed in the hashtable *in-domain*, this function
+;;; assumes the expression is defined on the intersection of the domains of its
+;;; arguments. So both addition and multiplication default to this case, for example.
+
 ;;; I would like to include a final call to standardize-inequality. But standardize-inequality
-;;; simplifies x^2+1 =/= 0 to true.
+;;; simplifies x^2+1 =/= 0 to true. That allows some spurious solutions to leak through, for
+;;; example, solve(x = 1/sqrt(x^2+1),x).
+
 (defun in-domain (e &optional (x nil))
 		(let ((fn (and (consp e) (consp (car e)) (gethash (caar e) *in-domain*))))
 				 (cond (($mapatom e) t)
